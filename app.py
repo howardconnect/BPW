b074a6cca421a5e61beb13e975e2999809fce693
howardconnect <howardconnect@gmail.com>
2025-06-17 18:24:22 -0400

    fixed the port issue

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def serve_static(path):
     return send_from_directory("static", path)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # 👈 Reads from env variable
+    app.run(host="0.0.0.0", port=port)
